45d802c8818a4c3014d5183a06af529cb89d2ef5
User <user@users-mac-mini.local>
2015-12-26 20:04:32 +0200

    InitialCommit

--- ButtonHomeCalculator/ViewController.h
+++ ButtonHomeCalculator/ViewController.h
@@ -7,9 +7,25 @@
 //
 
 #import <UIKit/UIKit.h>
-
+typedef enum{
+    AriphSum,
+    AriphMinus,
+    AriphMultipy,
+    AriphRozdel,
+    None
+}AriphmeticOperation;
 @interface ViewController : UIViewController
 
+- (IBAction)digitPressed:(UIButton *)sender forEvent:(UIEvent *)event;
+- (IBAction)plusPressed:(id)sender forEvent:(UIEvent *)event;
+- (IBAction)minusPressed:(id)sender forEvent:(UIEvent *)event;
+- (IBAction)multiplyPressed:(id)sender;
 
+- (IBAction)rozdelPressed:(id)sender;
+- (IBAction)resultPressed:(id)sender;
+@property (assign, nonatomic) AriphmeticOperation operation;
+@property (strong, nonatomic) IBOutlet UILabel *resultLabel;
+@property (assign,nonatomic) NSInteger currentResult;
+- (IBAction)clearPressed:(id)sender;
 @end
 
--- ButtonHomeCalculator/ViewController.m
+++ ButtonHomeCalculator/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.operation =None;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +25,57 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)digitPressed:(UIButton *)sender forEvent:(UIEvent *)event {
+    NSInteger digit = sender.tag;
+    self.resultLabel.text = [NSString stringWithFormat:@"%ld",(long)digit];
+    if (self.operation == None)
+    {
+        self.currentResult = digit;
+    }
+    else{
+        
+        switch (self.operation) {
+            case AriphMultipy:
+                self.currentResult = self.currentResult * digit;
+                break;
+            case AriphSum:
+                self.currentResult = self.currentResult + digit;
+                break;
+            case AriphRozdel:
+                self.currentResult = (NSInteger)self.currentResult / digit;
+                break;
+            default:
+                self.currentResult = self.currentResult - digit;
+                break;
+        }
+        self.operation = None;
+    }
+}
+
+- (IBAction)plusPressed:(id)sender forEvent:(UIEvent *)event {
+    NSLog(@"dsfsd");
+    self.operation = AriphSum;
+}
+
+- (IBAction)minusPressed:(id)sender forEvent:(UIEvent *)event {
+    self.operation = AriphMinus;
+}
+
+- (IBAction)multiplyPressed:(id)sender {
+    self.operation = AriphMultipy;
+}
+
+- (IBAction)rozdelPressed:(id)sender {
+    self.operation = AriphRozdel;
+}
+
+- (IBAction)resultPressed:(id)sender {
+    self.resultLabel.text = [NSString stringWithFormat:@"%ld",(long)self.currentResult];
+}
+
+- (IBAction)clearPressed:(id)sender {
+    self.resultLabel.text = @"0";
+    self.operation = None;
+    self.currentResult = 0;
+}
 @end
